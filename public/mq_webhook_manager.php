a48a73c64c72969482f4327dd9f2f3e7f205f4d0
benedictnkosi <nkosi.benedict@gmail.com>
2023-03-15 19:45:48 +0200

    defects created

--- public/mq_webhook_manager.php
+++ public/mq_webhook_manager.php
@@ -1,16 +1,13 @@
 <?php
 
+echo "test";
 $body = file_get_contents('php://input');
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,"https://ete.hotelrunner.co.za/no_auth/reservations/import/queuel");
+curl_setopt($ch, CURLOPT_URL,"https://ete.hotelrunner.co.za/no_auth/reservations/import/queue");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,
     "message=$body");
-
-// In real life you should use something like:
-// curl_setopt($ch, CURLOPT_POSTFIELDS,
-//          http_build_query(array('postvar1' => 'value1')));
 
 // Receive server response ...
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -18,3 +15,5 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $server_output = curl_exec($ch);
 
 curl_close($ch);
+
+echo "test2";
