e670c476c1cbd665907bfa0a4c80457d65bfab6a
benedictnkosi <nkosi.benedict@gmail.com>
2023-07-24 11:38:39 +0200

    fixed guest name defect

--- src/Controller/FilesController.php
+++ src/Controller/FilesController.php
@@ -23,18 +23,14 @@ class FilesController extends AbstractController
     /**
      * @Route("api/files/{name}")
      */
-    public function getFile($name, LoggerInterface $logger): BinaryFileResponse | JsonResponse
+    public function getFile($name, LoggerInterface $logger): BinaryFileResponse
     {
         $logger->info("Starting Method: " . __METHOD__);
         $documentDir = __DIR__ . '/../../files/';
         try{
             $file = new BinaryFileResponse($documentDir . $name);
         }catch(Exception){
-            $responseArray = array(
-                'result_message' => "File not found",
-                'result_code' => 1
-            );
-            return new JsonResponse($responseArray , 200, array());
+            $file = new BinaryFileResponse($documentDir . "empty.dat");
 
         }
         return $file;
