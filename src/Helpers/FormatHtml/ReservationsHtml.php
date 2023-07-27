6e17663d2c135479aa76eb62807f3e8c1eb35464
benedictnkosi <nkosi.benedict@gmail.com>
2023-07-27 16:18:24 +0200

    fixed guest name defect

--- src/Helpers/FormatHtml/ReservationsHtml.php
+++ src/Helpers/FormatHtml/ReservationsHtml.php
@@ -62,7 +62,7 @@ class ReservationsHtml
 									</div>';
 
         if($this->defectApi->isFunctionalityEnabled("download_reservations")) {
-            $htmlString .= '<a href="/'.$period . '_reservations.csv" target="_blank" download>Download CSV</a>
+            $htmlString .= '<a href="/'.$period . '_reservations.csv" target="_blank">Download CSV</a>
             <a href="/no_auth/room/dat/'.$period . '_reservations.dat" target="_blank">| Download Flat File</a>';
         }
 
